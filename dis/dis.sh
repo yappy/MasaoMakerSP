22c6d6f43fce98b0b7fb899db08fa40301f6af84
yappy <superunyo@yahoo.co.jp>
2024-12-25 21:41:37 +0900

    mc 2.8 と 3.0 をディスアセンブル

--- dis/dis.sh
+++ dis/dis.sh
@@ -2,5 +2,8 @@
 
 # Unzip mc_c.zip then disassemble all
 
-unzip -f ../original/files/mc_c.zip
-for file in ./*.class; do javap -v $file > ${file}.txt; done
+unzip -o ../original/mc2/mc_c.zip -d mc2
+for file in ./mc2/*.class; do javap -v $file > ${file}.txt; done
+
+unzip -o ../original/mc3/mc_c.zip -d mc3
+for file in ./mc3/*.class; do javap -v $file > ${file}.txt; done
