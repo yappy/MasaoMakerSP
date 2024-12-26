5785549404b3abdc7a067e2d5eb067e64d9962bf
yappy <superunyo@yahoo.co.jp>
2024-12-26 23:31:33 +0900

    音声再生のできない環境でも動くようにする
    例外を無視して最初の一回だけ表示する

--- mcport/src/main/java/io/github/yappy/mccport/AudioClipImpl.java
+++ mcport/src/main/java/io/github/yappy/mccport/AudioClipImpl.java
@@ -2,54 +2,68 @@ package io.github.yappy.mccport;
 
 import java.applet.AudioClip;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioClipImpl implements AudioClip {
 
-    private Clip clip;
+    private static AtomicBoolean errorOnce = new AtomicBoolean(false);
+    private Clip clip = null;
 
-    public AudioClipImpl(byte[] data) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-        var bin = new ByteArrayInputStream(data);
-        Clip clip = AudioSystem.getClip();
-        AudioFormat format = clip.getFormat();
-        // Clip supporting format PCM_SIGNED unknown sample rate, 16 bit, stereo, 4
-        // bytes/frame, big-endian is supported.
-        try (var ais = AudioSystem.getAudioInputStream(format, AudioSystem.getAudioInputStream(bin))) {
-            clip.open(ais);
-            this.clip = clip;
-        }
-        clip.addLineListener(ev -> {
-            if (ev.getType() == LineEvent.Type.STOP) {
-                Clip c = (Clip) ev.getSource();
-                c.stop();
-                c.setFramePosition(0);
+    public AudioClipImpl(byte[] data) {
+        try {
+            var bin = new ByteArrayInputStream(data);
+            // In headless env (no sound device),
+            // this method throws IllegalArgumentException (RuntimeException)
+            Clip clip = AudioSystem.getClip();
+            AudioFormat format = clip.getFormat();
+            // Clip supporting format PCM_SIGNED unknown sample rate, 16 bit, stereo, 4
+            // bytes/frame, big-endian is supported.
+            try (var ais = AudioSystem.getAudioInputStream(format, AudioSystem.getAudioInputStream(bin))) {
+                clip.open(ais);
+                this.clip = clip;
             }
-        });
+            clip.addLineListener(ev -> {
+                if (ev.getType() == LineEvent.Type.STOP) {
+                    Clip c = (Clip) ev.getSource();
+                    c.stop();
+                    c.setFramePosition(0);
+                }
+            });
+        } catch (Exception e) {
+            if (!errorOnce.getAndSet(true)) {
+                System.err.println("Failed to setup audio (this error is shown only once)");
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void play() {
         System.out.println("AudioClipImpl.play");
-        clip.start();
+        if (clip != null) {
+            clip.start();
+        }
     }
 
     @Override
     public void loop() {
         System.out.println("AudioClipImpl.loop");
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
     }
 
     @Override
     public void stop() {
         System.out.println("AudioClipImpl.stop");
-        clip.stop();
+        if (clip != null) {
+            clip.stop();
+        }
     }
 
 }
